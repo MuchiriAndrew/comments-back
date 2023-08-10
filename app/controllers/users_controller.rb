7503b68a5ce24a0b4d67243193fb6ebd75aa504e
Gerald-006 <jacksongeraldobere@gmail.com>
2023-08-11 01:18:38 +0300

    KK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     #   else
     #     render json: @user.errors, status: :unprocessable_entity
     #   end
-    user = User.new(user_params)
+    @user = User.new(user_params)
      if user.save
       session[:user_id] = user.id
       render json: { status: :created, user: user }
