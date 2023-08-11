9e9d9f17e66ec5e31b09305e34236a9a8077e478
MuchiriAndrew <kariukia225@gmail.com>
2023-08-11 11:34:16 +0300

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,22 +21,22 @@ class UsersController < ApplicationController
 
      # POST /replies
   def create
-    #   @user = User.new(user_params)
-    #   if @user.save
-    #     render json: @user, status: :created
-    #   else
-    #     render json: @user.errors, status: :unprocessable_entity
-    #   end
-    @user = User.new(user_params)
-     if user.save
-      session[:user_id] = user.id
-      render json: { status: :created, user: user }
-     else
-      render json: { errors: user.errors.full_messages, status: :unprocessable_entity }
-   end
+      @user = User.new(user_params)
+      if @user.save
+        render json: @user, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+  #   @user = User.new(user_params)
+  #    if user.save
+  #     session[:user_id] = user.id
+  #     render json: { status: :created, user: user }
+  #    else
+  #     render json: { errors: user.errors.full_messages, status: :unprocessable_entity }
+  #  end
  end
-     
-      
+
+
     private
 
     def user_params
